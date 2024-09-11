b945fe03f8eae5cda1e2fa715b5760c28c0c98d7
ARTURO RODRIGUEZ <arturo.rodriguez@utp.ac.pa>
2024-09-10 23:07:11 -0500

    Laboratorio 2 Completo

---
+++ Laboratorios/Laboratorio2/Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Laboratorio2 
+{ 
+        internal class Program
+        {
+            static void Main(string[] args) 
+            {
+                Client client = new Client();
+                client.FirstName = "Su_Nombre";
+                client.LastName = "Su_Apellido";
+                client.Age = "15";
+                client.Id = 1;
+
+                Console.WriteLine(client.GetFullName());
+            }    
+        }
+
+        public class Client
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Age { get; set; }
+        
+            public string GetFullName()
+            {  
+                return FirstName + " " + LastName;  
+            }
+        }
+
+}
