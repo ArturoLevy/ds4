ebcf879d90f82764e3cab09f72e2224eef486f6e
Arturo Rodríguez <120765362+ArturoLevy@users.noreply.github.com>
2024-09-29 22:37:13 -0500

    Laboratorio 5 completo

---
+++ Laboratorios/Laboratorio5/Laboratorio55/Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+{
+    {"Francia", "Paris"},
+    {"Espana", "Madrid"},
+    {"Italia", "Roma"}
+};
+
+foreach (KeyValuePair<string, string> par in paisesYCapitales)
+{
+    Console.WriteLine("La capital de " + par.Key + "es" + par.Value + ".");
+}
